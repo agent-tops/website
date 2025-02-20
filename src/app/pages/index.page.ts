80407bb7dc199d0c063523051f9f9e261077b402
Benny Tops <benny.tops@remecare.eu>
2025-02-20 21:50:34 +0100

    Add decent landing page

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,6 +1,13 @@
-import { RouteMeta } from '@analogjs/router';
+import { Component, signal } from '@angular/core';
 
-export const routeMeta : RouteMeta = {
-  redirectTo: '/blog',
-  pathMatch: 'full'
+@Component({
+  selector: 'app-home',
+  templateUrl: 'index.page.html'
+})
+export default class HomeComponent {
+  count = signal(0);
+
+  increment() {
+    this.count.update((count) => count + 1);
+  }
 }
