b1c268b435faecb2d6ebdc2f93798042fceb4e5e
Benny Tops <benny.tops@remecare.eu>
2025-03-02 12:42:29 +0100

    Fix top margin for mobile screens

--- src/app/pages/build.page.ts
+++ src/app/pages/build.page.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <section class="bg-white dark:bg-gray-900">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+      <div class="mx-auto max-w-screen-xl py-16 px-6">
+        <div class="mx-auto max-w-screen-sm text-center mb-16">
           <h2
             class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
           >
