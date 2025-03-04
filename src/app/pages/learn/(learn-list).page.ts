e46b7ced07fc03ebec7c37d9dcb8ffe79a81f8ac
Benny Tops <benny.tops@remecare.eu>
2025-03-04 19:57:36 +0100

    Make Coming Soon text smaller

--- src/app/pages/learn/(learn-list).page.ts
+++ src/app/pages/learn/(learn-list).page.ts
@@ -26,7 +26,7 @@ import ArticleTileComponent from '../../ui/article-tile.component';
     } @else {
     <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
       <h2
-        class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+        class="mb-4 text-xl lg:text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white"
       >
         Coming soon ...
       </h2>
