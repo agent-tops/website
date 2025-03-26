cdcdb96f2ef918b197c62f17aedecb63a57d607f
Benny Tops <benny.tops@remecare.eu>
2025-03-26 17:25:58 +0100

    Make image better fitting

--- src/app/ui/article-tile.component.ts
+++ src/app/ui/article-tile.component.ts
@@ -6,10 +6,10 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   template: `
   <a [routerLink]="[ basePath(), slug() ]" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg w-64 m-5 md:h-auto md:w-32 md:rounded-none md:rounded-s-lg" src="{{ image() }}" alt="">
+    <img class="object-contain w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ image() }}" alt="">
     <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ title() }}</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-100">{{ description() }}</p>
+        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ title() }}</h5>
+        <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-100">{{ description() }}</p> -->
     </div>
 </a>
   `,
